85078abb51ce621051c87e17a1eef47537cd2ddd
Subhash <nsubhash311@gmail.com>
2020-07-17 08:33:29 +0530

    Updates

---
+++ precache-manifest.27d8a4159c6ed89d10dd683dbf3cc864.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "614399dc90fc7669a46e615734cce292",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33633de7ceff76485806",
+    "url": "/static/css/2.c2b19279.chunk.css"
+  },
+  {
+    "revision": "aff4f6c3afca3f9a802b",
+    "url": "/static/css/main.57044b2b.chunk.css"
+  },
+  {
+    "revision": "33633de7ceff76485806",
+    "url": "/static/js/2.79908214.chunk.js"
+  },
+  {
+    "revision": "aff4f6c3afca3f9a802b",
+    "url": "/static/js/main.071914f7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
